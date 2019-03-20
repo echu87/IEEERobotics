b7d38c757dc1dd44caf7788fdb493ae7a32c8c1f
Evan Chu <echu1@ocdsb.ca>
2019-03-20 12:51:07 -0400

    added imports

---
+++ main/main.ino
@@ -0,0 +1,64 @@
+#include <Me4Button.h>
+#include <Me7SegmentDisplay.h>
+#include <MeAuriga.h>
+#include <MeBaseBoard.h>
+#include <MeBluetooth.h>
+#include <MeBuzzer.h>
+#include <MeColorSensor.h>
+#include <MeCompass.h>
+#include <MeConfig.h>
+#include <MeDCMotor.h>
+#include <MeEEPROM.h>
+#include <MeEncoderMotor.h>
+#include <MeEncoderNew.h>
+#include <MeEncoderOnBoard.h>
+#include <MeFlameSensor.h>
+#include <MeGasSensor.h>
+#include <MeGyro.h>
+#include <MeHostParser.h>
+#include <MeHumitureSensor.h>
+#include <MeInfraredReceiver.h>
+#include <MeIR.h>
+#include <MeJoystick.h>
+#include <MeLEDMatrix.h>
+#include <MeLEDMatrixData.h>
+#include <MeLightSensor.h>
+#include <MeLimitSwitch.h>
+#include <MeLineFollower.h>
+#include <MeMbotDCMotor.h>
+#include <MeMCore.h>
+#include <MeMegaPi.h>
+#include <MeMegaPiDCMotor.h>
+#include <MeMegaPiPro.h>
+#include <MeMegaPiPro4DcMotor.h>
+#include <MeMegaPiProESCMotor.h>
+#include <MeOnBoardTemp.h>
+#include <MeOneWire.h>
+#include <MeOrion.h>
+#include <MePIRMotionSensor.h>
+#include <MePm25Sensor.h>
+#include <MePort.h>
+#include <MePotentiometer.h>
+#include <MePressureSensor.h>
+#include <MePS2.h>
+#include <MeRGBLed.h>
+#include <MeSerial.h>
+#include <MeShield.h>
+#include <MeShutter.h>
+#include <MeSmartServo.h>
+#include <MeSoundSensor.h>
+#include <MeStepper.h>
+#include <MeStepperOnBoard.h>
+#include <MeSuperVariable.h>
+#include <MeTemperature.h>
+#include <MeTouchSensor.h>
+#include <MeUltrasonicSensor.h>
+#include <MeUSBHost.h>
+#include <MeVoice.h>
+#include <MeWifi.h>
+
+int ojas = 4;
+
+
+
+
